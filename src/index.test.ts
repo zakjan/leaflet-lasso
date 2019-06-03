f27dc6a51e28e364aefdaa3fb23ae31c6d545c2f
Jan Zak <zj@zakjan.cz>
2019-06-03 06:36:51 +0200

    Update tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -13,12 +13,22 @@ describe('index', () => {
         map = L.map(container, { renderer: new L.SVG(), center: [0, 0], zoom: 12 });
     });
 
-    it('exports LassoHandler', () => {
+    it('exports L.Lasso', () => {
+        const lassoHandler: L.Lasso = new L.Lasso(map);
+        expect(lassoHandler).toBeDefined();
+    });
+
+    it('exports L.lasso', () => {
         const lassoHandler: L.Lasso = L.lasso(map);
         expect(lassoHandler).toBeDefined();
     });
 
-    it('exports LassoControl', () => {
+    it('exports L.Control.Lasso', () => {
+        const lassoControl: L.Control.Lasso = new L.Control.Lasso().addTo(map);
+        expect(lassoControl).toBeDefined();
+    });
+
+    it('exports L.control.lasso', () => {
         const lassoControl: L.Control.Lasso = L.control.lasso().addTo(map);
         expect(lassoControl).toBeDefined();
     });
