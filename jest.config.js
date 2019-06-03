6da92232f6a23bb3b180dfa1ed94049244f37719
Jan Zak <zj@zakjan.cz>
2019-06-03 10:20:27 +0200

    Collect code coverage

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,14 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFiles: [
-      "<rootDir>/jest-pre.ts"
+    '<rootDir>/jest-pre.ts',
   ],
   moduleNameMapper: {
-    '\\.css$': 'identity-obj-proxy'
+    '\\.css$': 'identity-obj-proxy',
   },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.test.ts',
+  ],
 };
