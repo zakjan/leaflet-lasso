974edb3283fbfb1b7676ab2d438a9c587b98c1db
Jan Zak <zj@zakjan.cz>
2020-05-12 07:44:34 +0200

    Remove circle-to-polygon custom typings, moved to DefinitelyTyped

--- typings/circle-to-polygon.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'circle-to-polygon' {
-    const circleToPolygon: (center: GeoJSON.Position, radius: number, numberOfSegments?: number) => GeoJSON.Polygon;
-    export default circleToPolygon;
-}
