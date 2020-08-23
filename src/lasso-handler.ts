79a8076df5f3f2b883b1822442604f8fb7d57584
Jan Zak <zj@zakjan.cz>
2020-08-23 07:22:52 +0200

    Add support for touch events

--- src/lasso-handler.ts
+++ src/lasso-handler.ts
@@ -34,6 +34,7 @@ export class LassoHandler extends L.Handler {
 
     private polygon?: LassoPolygon;
 
+    private onMapMouseDownBound = this.onMapMouseDown.bind(this);
     private onDocumentMouseMoveBound = this.onDocumentMouseMove.bind(this);
     private onDocumentMouseUpBound = this.onDocumentMouseUp.bind(this);
 
@@ -58,7 +59,8 @@ export class LassoHandler extends L.Handler {
     
     addHooks() {
         this.map.getPane('mapPane');
-        this.map.on('mousedown', this.onMapMouseDown, this);
+        this.map.getContainer().addEventListener('mousedown', this.onMapMouseDownBound);
+        this.map.getContainer().addEventListener('touchstart', this.onMapMouseDownBound);
         
         const mapContainer = this.map.getContainer();
         mapContainer.classList.add(ACTIVE_CLASS);
@@ -73,9 +75,12 @@ export class LassoHandler extends L.Handler {
             this.polygon = undefined;
         }
 
-        this.map.off('mousedown', this.onMapMouseDown, this);
+        this.map.getContainer().removeEventListener('mousedown', this.onMapMouseDownBound);
+        this.map.getContainer().removeEventListener('touchstart', this.onMapMouseDownBound);
         document.removeEventListener('mousemove', this.onDocumentMouseMoveBound);
+        document.removeEventListener('touchmove', this.onDocumentMouseMoveBound);
         document.removeEventListener('mouseup', this.onDocumentMouseUpBound);
+        document.removeEventListener('touchend', this.onDocumentMouseUpBound);
 
         this.map.getContainer().classList.remove(ACTIVE_CLASS);
         document.body.classList.remove(ACTIVE_CLASS);
@@ -84,47 +89,78 @@ export class LassoHandler extends L.Handler {
         this.map.fire(DISABLED_EVENT);
     }
 
-    private onMapMouseDown(event: L.LeafletEvent) {
-        const event2 = event as L.LeafletMouseEvent;
+    private onMapMouseDown(event: Event) {
+        let event2 = this.eventToMouseEvent('down', event);
+        if (!event2) {
+            return;
+        }
 
         // activate lasso only for left mouse button click
-        if (event2.originalEvent.buttons !== 1) {
+        if (event instanceof MouseEvent && event.buttons !== 1) {
             this.disable();
             return;
         }
 
         // skip clicks on controls
-        if ((event2.originalEvent.target as HTMLElement).closest('.leaflet-control-container')) {
+        if ((event.target as HTMLElement).closest('.leaflet-control-container')) {
             return;
         }
 
-        this.polygon = new LassoPolygon([event2.latlng], this.options.polygon).addTo(this.map);
+        const latLng = this.map.mouseEventToLatLng(event2);
+        this.polygon = new LassoPolygon([latLng], this.options.polygon).addTo(this.map);
 
         document.body.classList.add(ACTIVE_CLASS);
 
         document.addEventListener('mousemove', this.onDocumentMouseMoveBound);
+        document.addEventListener('touchmove', this.onDocumentMouseMoveBound);
         document.addEventListener('mouseup', this.onDocumentMouseUpBound);
+        document.addEventListener('touchend', this.onDocumentMouseUpBound);
+
+        event.preventDefault();
     }
 
     private onDocumentMouseMove(event: Event) {
+        let event2 = this.eventToMouseEvent('move', event);
+        if (!event2) {
+            return;
+        }
+
         if (!this.polygon) {
             return;
         }
 
-        const event2 = event as MouseEvent;
-
         // keep lasso active only if left mouse button is hold
-        if (event2.buttons !== 1) {
+        if (event instanceof MouseEvent && event.buttons !== 1) {
             console.warn('mouseup event was missed');
             this.finish();
             return;
         }
 
-        this.polygon.addLatLng(this.map.mouseEventToLatLng(event2));
+        const latLng = this.map.mouseEventToLatLng(event2);
+        this.polygon.addLatLng(latLng);
+
+        event.preventDefault();
     }
 
-    private onDocumentMouseUp() {
+    private onDocumentMouseUp(event: Event) {
         this.finish();
+
+        event.preventDefault();
+    }
+
+    private eventToMouseEvent(type: string, event: Event) {
+        if (event instanceof MouseEvent) {
+            return event;
+        }
+        if (event instanceof TouchEvent && event.touches.length === 1) {
+            return new MouseEvent(type, {
+                screenX: event.touches[0].screenX,
+                screenY: event.touches[0].screenY,
+                clientX: event.touches[0].clientX,
+                clientY: event.touches[0].clientY,
+                buttons: 1,
+            });
+        }
     }
 
     private finish() {
